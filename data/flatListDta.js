7a03ecb6a5b644fd891b711a76061fbf314c02cf
Your Name <you@example.com>
2020-10-26 20:23:36 +0700

    first commit

---
+++ data/flatListDta.js
@@ -0,0 +1,51 @@
+var flatListData = [
+    {
+        "key": "1",
+        "name": "áo thun nam",
+        "imageUrl": "https://bucket.nhanh.vn/store/7136/ps/20201019/IMG_6317_Doi_thumb.jpg",
+        "mota": "385.000đ"
+    },
+    {
+        "key": "2",
+        "name": "quần thể thao",
+        "imageUrl": "https://bucket.nhanh.vn/store/7136/ps/20201019/IMG_1900_thumb.jpg",
+        "mota": "385.000₫"
+    },
+    {
+        "key": "3",
+        "name": "quần jean nam",
+        "imageUrl": "https://bucket.nhanh.vn/store/7136/ps/20201019/IMG_1897_thumb.jpg",
+        "mota": "285.000đ"
+    },
+    {
+        "key": "4",
+        "name": "áo sơ mi",
+        "imageUrl": "https://bucket.nhanh.vn/store/7136/ps/20201019/IMG_6351_thumb.jpg",
+        "mota": "265.000₫"
+    },
+    {
+        "key": "5",
+        "name": "áo phông",
+        "imageUrl": "https://bucket.nhanh.vn/store/7136/ps/20201015/IMG_6264_thumb.jpg",
+        "mota": "399,000đ"
+    },
+    {
+        "key": "6",
+        "name": "quần thể thao",
+        "imageUrl": "https://bucket.nhanh.vn/store/7136/ps/20201019/IMG_6266_thumb.jpg",
+        "mota": "400.000₫"
+    },
+    {
+        "key": "7",
+        "name": "áo phông",
+        "imageUrl": "https://bucket.nhanh.vn/store/7136/ps/20200914/D2T9_U1ATN090002_AO%20THUN%20DOI_TOTOSHOP%20(2)_thumb.jpg",
+        "mota": "300,000đ"
+    },
+    {
+        "key": "8",
+        "name": "quần thun",
+        "imageUrl": "https://bucket.nhanh.vn/store/7136/ps/20200912/D2T9_M1QJG090001_QUAN%20JOGGER%20DU_NAM_NU_TOTOSHOP%20(3)_thumb.jpg",
+        "mota": "485,000đ"
+    },
+];
+module.exports = flatListData;
